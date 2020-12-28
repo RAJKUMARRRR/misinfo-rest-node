4261ce7a41c9de3fb373f30628235138cfc42788
rchinthala-ebsco <75062625+rchinthala-ebsco@users.noreply.github.com>
2020-12-28 22:28:51 +0530

    changed mongodb uri connection

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3001,
+  //mongoUri: process.env.MONGODB_URI ||
+    //'mongodb://sampath:sampath123@ds231377.mlab.com:31377/misinfo',
   mongoUri: process.env.MONGODB_URI ||
-    'mongodb://sampath:sampath123@ds231377.mlab.com:31377/misinfo',
+    'mongodb+srv://misinfoproduction:Raj@8501096987@cluster0.5vtq0.mongodb.net/misinfo?retryWrites=true&w=majority',
   serverUrl: process.env.serverUrl || 'http://localhost:3001'
 }
 
